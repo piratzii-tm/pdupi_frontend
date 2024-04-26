a9bd88eb39a734ac559548083feebe0d0814c5e5
Iulian Pop <iulian.pop@cobaltsign.com>
2024-04-26 11:44:26 +0300

    Init the style and implement the button component

---
+++ src/constants/theming.js
@@ -0,0 +1,11 @@
+export const Colors = {
+  barberry: "#E5DD0E",
+  cod_gray: "#111111",
+  tuatara: "#2D2D2C",
+};
+
+export const Fonts = {
+  koulen: ["Koulen", "sans-serif"],
+  mohave: ["Mohave", "sans-serif"],
+  montserrat: ["Montserrat", "sans-serif"],
+};
